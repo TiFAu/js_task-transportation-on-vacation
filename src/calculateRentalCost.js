3e893635b40b1273c9dcd451ef19f9a4cb552d6a
TiFAu <turchin.anton2@gmail.com>
2025-11-22 23:07:11 +0200

    Solution

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -5,14 +5,18 @@
  */
 function calculateRentalCost(days) {
   const DAYRENT = 40;
-  let rentalCost = DAYRENT * days;
+  const LONG_RENTAL_PERIOD_DAYS = 7;
+  const LONG_RENTAL_DISCOUNT = 50;
+  const SHORT_RENTAL_PERIOD_DAYS = 3;
+  const SHORT_RENTAL_DISCOUNT = 20;
+  const rentalCost = DAYRENT * days;
 
-  if (days >= 7) {
-    return rentalCost - 50;
+  if (days >= LONG_RENTAL_PERIOD_DAYS) {
+    return rentalCost - LONG_RENTAL_DISCOUNT;
   }
 
-  if (days >= 3) {
-    return rentalCost - 20;
+  if (days >= SHORT_RENTAL_PERIOD_DAYS) {
+    return rentalCost - SHORT_RENTAL_DISCOUNT;
   }
 
   return rentalCost;
